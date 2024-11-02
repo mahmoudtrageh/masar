fc3220e888800bb65aa03e15ffea4d3b465744ab
mahmoudtrageh <geo.mahmoudtaha@gmail.com>
2024-11-02 14:29:47 +0200

    fix logged in

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,31 +20,33 @@
                                     {{ $category->title }}
                                 </a>
                             </h2>
-                            <a href="#" class="ml-4">
-                                @if(auth()->user()->favourites()->where(['favouritable_id' => $category->id, 'favouritable_type' => App\Models\Category::class])->exists())
-                                    <form action="{{ route('favourites.destroy', ['type' => 'category', 'id' => $category->id]) }}" method="POST" class="inline">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="text-red-500"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                                            <path
-                                                d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
-                                                fill="red"
-                                            />
-                                        </svg></button>
-                                    </form>
-                                @else
-                                    <form action="{{ route('favourites.store', ['type' => 'category', 'id' => $category->id]) }}" method="POST" class="inline">
-                                        @csrf
-                                        <button type="submit" class="text-blue-500"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                                            <path
-                                                d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
-                                                fill="grey"
-                                            />
-                                        </svg></button>
-                                    </form>
-                                @endif
+                            @if(auth()->user())
+                                <a href="#" class="ml-4">
+                                    @if(auth()->user()->favourites()->where(['favouritable_id' => $category->id, 'favouritable_type' => App\Models\Category::class])->exists())
+                                        <form action="{{ route('favourites.destroy', ['type' => 'category', 'id' => $category->id]) }}" method="POST" class="inline">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="text-red-500"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                                <path
+                                                    d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+                                                    fill="red"
+                                                />
+                                            </svg></button>
+                                        </form>
+                                    @else
+                                        <form action="{{ route('favourites.store', ['type' => 'category', 'id' => $category->id]) }}" method="POST" class="inline">
+                                            @csrf
+                                            <button type="submit" class="text-blue-500"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                                <path
+                                                    d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+                                                    fill="grey"
+                                                />
+                                            </svg></button>
+                                        </form>
+                                    @endif
 
-                            </a>
+                                </a>
+                            @endif
                         </div>
 
                         <p class="mt-4 text-sm/relaxed">
